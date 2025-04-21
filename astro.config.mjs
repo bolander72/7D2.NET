bae0c99952a42585baa95d52cbb3df0e47acfc43
Michael Boland <23284377+bolander72@users.noreply.github.com>
2025-04-20 20:02:58 -0500

    Add landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [sitemap()],
     output: "static",
-    adapter: vercel()
+    adapter: vercel(),
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': '/src'
+            }
+        }
+    },
 });
